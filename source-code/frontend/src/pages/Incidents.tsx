5f41951c67a406b4bfa89f81739c329bbb9b683f
Mattrfish <146200948+Mattrfish@users.noreply.github.com>
2025-11-26 15:19:08 +0000

    Move Incidents and Settings to separate pages
    Refactored Incidents and Settings from inline placeholder components in App.tsx to dedicated files under src/pages. Updated imports in App.tsx and removed unused state logic from NavBar.tsx.

---
+++ source-code/frontend/src/pages/Incidents.tsx
@@ -0,0 +1,10 @@
+export default function Incidents(){
+    return(
+        <div className="p-6 space-y-6">
+            <h2 className="text 2x1 font-semibold">Analytics Overview</h2>
+            <p>List of incidents goes here...</p>
+        </div>
+    );
+
+}
+
